f5fbf52d4c62679c85585587f91f337f038ac8ed
iam-nazmul <nazmul.cse48@gmail.com>
2026-09-01 12:39:41 +0600

    .env.example created

--- app/main.py
+++ app/main.py
@@ -3,16 +3,26 @@ import sys
 import argparse
 from openai import OpenAI
 
-
 from dotenv import load_dotenv
-
 load_dotenv()
+
+
+API_KEY = os.getenv("OPENROUTER_API_KEY")
+BASE_URL = os.getenv("OPENROUTER_BASE_URL", default="https://openrouter.ai/api/v1")
 
 
 
 
 def main():
-    print("Hello from karigor!")
+    p = argparse.ArgumentParser()
+    p.add_argument("-p", required=True)
+    args = p.parse_args()
+   
+
+    if not API_KEY:
+        print("Error: OPENROUTER_API_KEY is not set in the environment variables.")
+
+    
 
 
 if __name__ == "__main__":
